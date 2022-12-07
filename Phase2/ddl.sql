33c6a82a96751166b223a9156e1a4cd65d187a01
Even Pham <even.pham11@gmail.com>
2022-12-07 17:17:10 -0500

    Update ddl.sql

--- Phase2/ddl.sql
+++ Phase2/ddl.sql
@@ -37,8 +37,7 @@ create table if not EXISTS plants(
     primary key (plant_ID),
     foreign key (store_ID) references store(store_ID),
     constraint CHK_plant check (plant_size in ('XS','S','M','L') 
-        and plant_warning in ('Safe', 'Poisonous Sap', 'Allergies', 'Not Pet Safe', 'Not Child Safe', 'Not Pet Safe + Not Child Safe', 'Not Pet Safe + Not Child Safe + Poisonous Sap','Not Pet Safe + Not Child Safe + Allergies + Poisonous Sap')
-        --('pet safe','child safe' ,'poisonous sap','not pet safe ','not child safe' ,'safe (both pet and child)','pollen','dust')                      
+        and plant_warning in ('Safe', 'Poisonous Sap', 'Allergies', 'Not Pet Safe', 'Not Child Safe', 'Not Pet Safe + Not Child Safe', 'Not Pet Safe + Not Child Safe + Poisonous Sap','Not Pet Safe + Not Child Safe + Allergies + Poisonous Sap')                     
         and plant_species in ('cacti', 'fern', 'palm', 'flowering', 'succulent', 'common house'))
 );
 
@@ -104,75 +103,75 @@ VALUES
 
 INSERT into plants(plant_ID, plant_name, plant_species, plant_color, plant_climate_zone, plant_difficulty, plant_light, plant_water, plant_price_range, plant_size, plant_warning, store_ID)
 VALUES
-(1, 'Cereus', 'Cacti', 'Green', '3', 5, 'direct', 3, '$$', 'L', 'Safe', 1)
-(2, 'Ferocactus', 'Cacti', 'Green', '3', 2, 'direct', 2, '$', 'M', 'Safe', 1)
-(3, 'Jade', 'Succulent', 'Green', '1,2,3', 4, 'shade', 2, '$', 'XS', 'Poisonous Sap', 3)
-(4, 'Burros Tail', 'Succulent', 'Green', '1,2,3', 7, 'direct', 2, '$', 'M', 'Safe', 2)
-(5, 'Parlor Palm', 'Palm', 'Green', '1,2,3', 4, 'shade', 2, '$', 'M', 'Not Pet Safe', 1)
-(6, 'Majesty Palm', 'Palm', 'Green', '1,2,3', 5, 'shade', 2, '$$', 'L', 'Safe', 3)
-(7, 'African Violet', 'Flowering', 'Purple', '2,3', 6, 'indirect', 1, '$', 'S', 'Allergies', 2)
-(8, 'Pink Anthurium', 'Flowering', 'Pink', '2,3', 3, 'indirect', 1, '$$$', 'S', 'Not Pet Safe + Not Child Safe + Allergies + Poisonous Sap', 5)
-(9, 'Delphinum', 'Flowering', 'Blue', '4,5,6,7', 7, 'direct', 1, '$$', 'L', 'Not Pet Safe + Not Child Safe', 4)
-(10, 'Staghorn Fern', 'Fern', 'Purple', '1,2,3', 8, 'indirect', 1, '$', 'M', 'Safe', 3)
-(11, 'Alocasia', 'Common House', 'Purple', '2,3,4', 5, 'indirect', 1, '$$', 'L', 'Not Pet Safe + Not Child Safe', 4)
-(12, 'Monstera', 'Common House', 'Freen', '1,2,3,4', 3, 'indirect', 2, '$$', 'L', 'Not Pet Safe + Not Child Safe + Poisonous Sap', 6)
-(13, 'Philodendron Pathos', 'Common House', 'Green', '1,2,3', 2, 'indirect', 2, '$$', 'M', 'Not Pet Safe + Not Child Safe', 5)
-(14, 'Fiddle Leaf Fig', 'Common House', 'Green', '1,2,3', 7, 'direct', 1, '$$', 'L', 'Not Pet Safe + Not Child Safe + Poisonous Sap', 4)
+(1, 'Cereus', 'Cacti', 'Green', '3', 5, 'direct', 3, '$$', 'L', 'Safe', 1),
+(2, 'Ferocactus', 'Cacti', 'Green', '3', 2, 'direct', 2, '$', 'M', 'Safe', 1),
+(3, 'Jade', 'Succulent', 'Green', '1,2,3', 4, 'shade', 2, '$', 'XS', 'Poisonous Sap', 3),
+(4, 'Burros Tail', 'Succulent', 'Green', '1,2,3', 7, 'direct', 2, '$', 'M', 'Safe', 2),
+(5, 'Parlor Palm', 'Palm', 'Green', '1,2,3', 4, 'shade', 2, '$', 'M', 'Not Pet Safe', 1),
+(6, 'Majesty Palm', 'Palm', 'Green', '1,2,3', 5, 'shade', 2, '$$', 'L', 'Safe', 3),
+(7, 'African Violet', 'Flowering', 'Purple', '2,3', 6, 'indirect', 1, '$', 'S', 'Allergies', 2),
+(8, 'Pink Anthurium', 'Flowering', 'Pink', '2,3', 3, 'indirect', 1, '$$$', 'S', 'Not Pet Safe + Not Child Safe + Allergies + Poisonous Sap', 5),
+(9, 'Delphinum', 'Flowering', 'Blue', '4,5,6,7', 7, 'direct', 1, '$$', 'L', 'Not Pet Safe + Not Child Safe', 4),
+(10, 'Staghorn Fern', 'Fern', 'Purple', '1,2,3', 8, 'indirect', 1, '$', 'M', 'Safe', 3),
+(11, 'Alocasia', 'Common House', 'Purple', '2,3,4', 5, 'indirect', 1, '$$', 'L', 'Not Pet Safe + Not Child Safe', 4),
+(12, 'Monstera', 'Common House', 'Freen', '1,2,3,4', 3, 'indirect', 2, '$$', 'L', 'Not Pet Safe + Not Child Safe + Poisonous Sap', 6),
+(13, 'Philodendron Pathos', 'Common House', 'Green', '1,2,3', 2, 'indirect', 2, '$$', 'M', 'Not Pet Safe + Not Child Safe', 5),
+(14, 'Fiddle Leaf Fig', 'Common House', 'Green', '1,2,3', 7, 'direct', 1, '$$', 'L', 'Not Pet Safe + Not Child Safe + Poisonous Sap', 4),
 (15, 'Pilea Pepermoidies', 'Common House', 'Green', '1,2,3,4', 4, 'indirect', 1, '$', 'M', 'Allergies', 6)
 ;
 
 INSERT into climate_zones(zone_state, climate_zone)
 VALUES
-('AS', 1)
-('GU', 1)
-('HI', 1)
-('MP', 1)
-('PR', 1)
-('UM', 1)
-('FI', 2)
-('TX', 2)
-('AL', 3)
-('AR', 3)
-('CA', 3)
-('GA', 3)
-('LA', 3)
-('MS', 3)
-('NC', 3)
-('OK', 3)
-('SC', 3)
-('VA', 4)
-('DE', 4)
-('IN', 4)
-('KS', 4)
-('KY', 4)
-('MD', 4)
-('MO', 4)
-('NJ', 4)
-('OR', 4)
-('TN', 4)
-('VT', 4)
-('IL', 5)
-('NE', 5)
-('NH', 5)
-('NM', 5)
-('NV', 5)
-('OH', 5)
-('PA', 5)
-('RI', 5)
-('WA', 5)
-('WV', 5)
-('IA', 6)
-('ID', 6)
-('ME', 6)
-('MI', 6)
-('MN', 6)
-('MT', 6)
-('ND', 6)
-('NY', 6)
-('SD', 6)
-('UT', 6)
-('WI', 6)
-('WY', 6)
+('AS', 1),
+('GU', 1),
+('HI', 1),
+('MP', 1),
+('PR', 1),
+('UM', 1),
+('FI', 2),
+('TX', 2),
+('AL', 3),
+('AR', 3),
+('CA', 3),
+('GA', 3),
+('LA', 3),
+('MS', 3),
+('NC', 3),
+('OK', 3),
+('SC', 3),
+('VA', 4),
+('DE', 4),
+('IN', 4),
+('KS', 4),
+('KY', 4),
+('MD', 4),
+('MO', 4),
+('NJ', 4),
+('OR', 4),
+('TN', 4),
+('VT', 4),
+('IL', 5),
+('NE', 5),
+('NH', 5),
+('NM', 5),
+('NV', 5),
+('OH', 5),
+('PA', 5),
+('RI', 5),
+('WA', 5),
+('WV', 5),
+('IA', 6),
+('ID', 6),
+('ME', 6),
+('MI', 6),
+('MN', 6),
+('MT', 6),
+('ND', 6),
+('NY', 6),
+('SD', 6),
+('UT', 6),
+('WI', 6),
+('WY', 6),
 ('AK', 7)
 ;
 
@@ -180,32 +179,32 @@ VALUES
 
 INSERT into homes(home_id, state, avg_temperature, sunlight, pets, kids, frequency)
 VALUES
-(1, 'VA', 72, 'direct', 3, 2, 6)
-(2, 'MA', 52, 'shade', 0, 1, 5)
-(3, 'MD', 70, 'indirect', 0, 0, 2)
-(4, 'CA', 80, 'direct', 4, 1, 6)
-(5, 'CO', 69, 'shade', 2, 2, 7)
+(1, 'VA', 72, 'direct', 3, 2, 6),
+(2, 'MA', 52, 'shade', 0, 1, 5),
+(3, 'MD', 70, 'indirect', 0, 0, 2),
+(4, 'CA', 80, 'direct', 4, 1, 6),
+(5, 'CO', 69, 'shade', 2, 2, 7),
 (6, 'MA', 60, 'shade', 1, 3, 7)
 ;
 
 
 
-INSERT into recipents(recipients_id, recipients_first_name, recipients_home_id, recipients_plant_id, email)
+INSERT into recipients(recipients_id, recipients_first_name, recipients_home_id, recipients_plant_id, email)
 VALUES
-(1, 'Annie', 1, 4, 'sasculia@omdiaco.com')
-(2, 'John', 2, 4, 'eingekochtes@nakiuha.com')
-(3, 'Cena', 3, 4, 'dmorufi@kasmabirader.com')
-(4, 'Mateo', 4, 4, 'ninahoegele@nakiuha.com')
-(5, 'Lin', 1, 5, 'mlwick@acampadaparis.com')
+(1, 'Annie', 1, 4, 'sasculia@omdiaco.com'),
+(2, 'John', 2, 4, 'eingekochtes@nakiuha.com'),
+(3, 'Cena', 3, 4, 'dmorufi@kasmabirader.com'),
+(4, 'Mateo', 4, 4, 'ninahoegele@nakiuha.com'),
+(5, 'Lin', 1, 5, 'mlwick@acampadaparis.com'),
 (6, 'Mohammud', 6, 4, 'gilmarsanteiro@cuendita.com')
 ;
 
 INSERT into emails(email_id, email_recpients_id, email_subject, date_reminder, email_body)
 VALUES
-(1, 1, 'Reminder', 3-23-2022)
-(2, 2, 'Plant Bought', 1-20-2022)
-(3, 3, 'Reminder', 2-17-2022)
-(4, 4, 'Reminder', 7-14-2022)
-(5, 5, 'Reminder', 8-4-2022)
-(6, 6, 'Reminder', 8-19-2022)
+(1, 1, 'Reminder', '2022-3-23',"bac"),
+(2, 2, 'Plant Bought', '2022-1-20',"asdf"),
+(3, 3, 'Reminder','2022-2-17',"absdv"),
+(4, 4, 'Reminder', '2022-7-14',"asdfadsf"),
+(5, 5, 'Reminder', '2022-8-4',"asdfadsf"),
+(6, 6, 'Reminder', '2022-8-19',"sdfasdfasdfasdf")
 ;
