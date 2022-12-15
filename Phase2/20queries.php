5874e0598c6727321cc01f17ec0d880c48a56399
Khongorzul Munkhgerel <90788062+kmunkhgerel@users.noreply.github.com>
2022-12-14 23:11:45 -0500

    adding 20 queries

---
+++ Phase2/20queries.php
@@ -0,0 +1,519 @@
+<!doctype html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function(){
+    $("#flip").click(function(){
+    $("#panel").slideToggle("slow");
+    });
+    });
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function(){
+    $("#flip2").click(function(){
+    $("#panel2").slideToggle("slow");
+    });
+    });
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function(){
+    $("#flip3").click(function(){
+    $("#panel3").slideToggle("slow");
+    });
+    });
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function(){
+    $("#flip4").click(function(){
+    $("#panel4").slideToggle("slow");
+    });
+    });
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function(){
+    $("#flip5").click(function(){
+    $("#panel5").slideToggle("slow");
+    });
+    });
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function(){
+    $("#flip6").click(function(){
+    $("#panel6").slideToggle("slow");
+    });
+    });
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function(){
+    $("#flip7").click(function(){
+    $("#panel7").slideToggle("slow");
+    });
+    });
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function(){
+    $("#flip8").click(function(){
+    $("#panel8").slideToggle("slow");
+    });
+    });
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function(){
+    $("#flip9").click(function(){
+    $("#panel9").slideToggle("slow");
+    });
+    });
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function(){
+    $("#flip10").click(function(){
+    $("#panel10").slideToggle("slow");
+    });
+    });
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function(){
+    $("#flip11").click(function(){
+    $("#panel11").slideToggle("slow");
+    });
+    });
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function(){
+    $("#flip12").click(function(){
+    $("#panel12").slideToggle("slow");
+    });
+    });
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function(){
+    $("#flip13").click(function(){
+    $("#panel13").slideToggle("slow");
+    });
+    });
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function(){
+    $("#flip14").click(function(){
+    $("#panel14").slideToggle("slow");
+    });
+    });
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function(){
+    $("#flip15").click(function(){
+    $("#panel15").slideToggle("slow");
+    });
+    });
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function(){
+    $("#flip16").click(function(){
+    $("#panel16").slideToggle("slow");
+    });
+    });
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function(){
+    $("#flip17").click(function(){
+    $("#panel17").slideToggle("slow");
+    });
+    });
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function(){
+    $("#flip18").click(function(){
+    $("#panel18").slideToggle("slow");
+    });
+    });
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function(){
+    $("#flip19").click(function(){
+    $("#panel19").slideToggle("slow");
+    });
+    });
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script> 
+    $(document).ready(function(){
+    $("#flip20").click(function(){
+    $("#panel20").slideToggle("slow");
+    });
+    });
+    </script>
+
+
+<style> 
+h1 {
+    text-align: center;
+    font-size: 20px; 
+    margin-top: 5px;
+    margin-bottom: 5px;
+    color: #000000;
+}
+
+h2 {
+    text-align: center;
+    margin-bottom: 2px;
+    font-size: 15px;
+    color: #FFFFFF;
+}
+
+h3 {
+    text-align: left;
+    margin-bottom: 2px;
+    font-size: 15px;
+    color: #FFFFFF;
+    padding-left: 10px;
+}
+
+#panel, #flip, #panel2, #flip2, #panel3, #flip3, #panel4, #flip4, 
+#panel5, #flip5, #panel6, #flip6, #panel7, #flip7, #panel8, #flip8, 
+#panel9, #flip9, #panel10, #flip10, #panel11, #flip11, #panel12, #flip12, 
+#panel13, #flip13, #panel14, #flip14, #panel15, #flip15, #panel16, #flip16, 
+#panel17, #flip17, #panel18, #flip18, #panel19, #flip19, #panel20, #flip20 {
+  padding: 5px;
+  text-align: center;
+  background-color: #4CAF50;
+  border: solid 1px #FFFFFF;
+}
+
+#panel, #panel2, #panel3, #panel4, #panel5, #panel6, #panel7, #panel8, #panel9, #panel10,
+#panel11, #panel12, #panel13, #panel14, #panel15, #panel16, #panel17, #panel18, #panel19, #panel20  {
+  padding: 5px;
+  display: none;
+  text-align:left;
+}
+
+
+table {
+  border-collapse: collapse;
+  width: 100%;
+  color: #FFFFFF;
+}
+
+td, th {
+  border: 1px solid #FFFFFF;
+  text-align: left;
+  padding: 8px;
+}
+</style>
+</head>
+
+<body> 
+<h1> Here are our 20 queries from Phase 1 on our Sample data</h1> 
+
+<div id="flip"><h2>Query 1: Display a list of recipients name and email</h2></div>
+<div id="panel">
+    <h2>SQL Code: </h2>
+    <h3>select <br>
+    &nbsp;&nbsp;recipients_first_name,  <br>
+    &nbsp;&nbsp; email  <br>
+    from <br> 
+    &nbsp;&nbsp;recipients  <br>
+    order by  <br>
+    &nbsp;&nbsp;recipients_first_name ASC  <br>
+    ;
+    </h3>
+    <h2>Results: </h2>
+    <table>
+        <tr>
+            <th>Recipients First Name</th>
+            <th>Email</th>
+        </tr>
+        <tr>
+            <td>Annie</td>
+            <td>sasculia@omdiaco.com</td>            
+        </tr>
+        <tr>
+            <td>Cena</td>
+            <td>dmorufi@kasmabirader.com</td>            
+        </tr>
+        <tr>
+            <td>John</td>
+            <td>eingekochtes@nakiuha.com</td>            
+        </tr>
+        <tr>
+            <td>Lin</td>
+            <td>mlwick@acampadaparis.com</td>            
+        </tr>
+        <tr>
+            <td>Mateo</td>
+            <td>ninahoegele@nakiuha.com</td>            
+        </tr>
+        <tr>
+            <td>Mohammud</td>
+            <td>gilmarsanteiro@cuendita.com</td>            
+        </tr>
+    </table>
+</div>
+
+<div id="flip2"><h2>Query 2: Display a list of small plants</h2></div>
+<div id="panel2">
+    <h2>SQL Code: </h2>
+    <h3>select <br>
+    &nbsp;&nbsp;plant_ID,  <br>
+    &nbsp;&nbsp;plant_name  <br>
+    from <br> 
+    &nbsp;&nbsp;plants  <br>
+    where <br>
+    &nbsp;&nbsp;plant-szie= 'S' <br>
+    ;
+    </h3>
+    <h2>Results: </h2>
+    <table>
+        <tr>
+            <th>Plant_ID</th>
+            <th>Plant_name</th>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>African Violet</td>            
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Pink Anthurium</td>            
+        </tr>
+    </table>
+</div>
+
+<div id="flip3"><h2>Query 3: Display a list of easy difficulty plants</h2></div>
+<div id="panel3">
+    <h2>SQL Code: </h2>
+    <h3>select <br>
+    &nbsp;&nbsp;plant_ID,  <br>
+    &nbsp;&nbsp;plant_name  <br>
+    from <br> 
+    &nbsp;&nbsp;plants  <br>
+    where <br>
+    &nbsp;&nbsp;plant_diffuclty= '3' <br>
+    &nbsp;&nbsp;or <br>
+    &nbsp;&nbsp;plant_diffuclty= '4' 
+    ;
+    </h3>
+    <h2>Results: </h2>
+    <table>
+        <tr>
+            <th>Plant ID</th>
+            <th>Plant Name</th>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Jade</td>            
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Pink Anthurium</td>            
+        </tr>
+        <tr>
+            <td>12</td>
+            <td>Monstera</td>            
+        </tr>
+        <tr>
+            <td>15</td>
+            <td>Pilea Pepermoidies</td>            
+        </tr>
+    </table>
+</div>
+
+<div id="flip4"><h2>Query 4: Display a list of recipients who live in Virginia state</h2></div>
+<div id="panel4">
+    <h2>SQL Code: </h2>
+    <h3>select <br>
+    &nbsp;&nbsp;recipients_id, <br>
+    &nbsp;&nbsp;recipients_first_name <br> 
+    from 
+    &nbsp;&nbsp;recipients <br>
+    &nbsp;&nbsp;&nbsp;&nbsp;join homes on (recipients_home_id = home_id) <br>
+    where <br>
+    &nbsp;&nbsp;state = 'VA' <br>
+    ;
+    </h3>
+    <h2>Results: </h2>
+    <table>
+        <tr>
+            <th>Recipients ID</th>
+            <th>Recipients First Name</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Annie</td>            
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Lin</td>            
+        </tr>
+    </table>
+</div>
+
+<div id="flip18"><h2>Query 18: Display the name and plant ID of the plants that need indirect lighting</h2></div>
+<div id="panel18">
+    <h2>SQL Code: </h2>
+    <h3>select <br>
+    &nbsp;&nbsp;plant_ID,  <br>
+    &nbsp;&nbsp;plant_name  <br>
+    from <br> 
+    &nbsp;&nbsp;plants  <br>
+    where <br>
+    &nbsp;&nbsp;plant_light = 'indirect' <br>
+    order by <br>
+    &nbsp;&nbsp;plant_ID <br>
+    ;
+    </h3>
+    <h2>Results: </h2>
+    <table>
+        <tr>
+            <th>Plant ID</th>
+            <th>Plant Name</th>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>African Violet</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Pink Anthurium</td>
+        </tr>
+        <tr>
+            <td>10</td>
+            <td>Staghorn Fern</td>
+        </tr>
+        <tr>
+            <td>11</td>
+            <td>Alocasia</td>
+        </tr>
+        <tr>
+            <td>12</td>
+            <td>Monstera</td>
+        </tr>
+        <tr>
+            <td>13</td>
+            <td>Philodendron Pathos</td>
+        </tr>
+        <tr>
+            <td>15</td>
+            <td>Pilea Pepermoidies</td>
+        </tr>
+    </table>
+</div>
+
+<div id="flip19"><h2>Query 19: Display body of the emails sent between July 2022 and August 2022</h2></div>
+<div id="panel19">
+    <h2>SQL Code: </h2>
+    <h3>select <br>
+    &nbsp;&nbsp;email_id,  <br>
+    &nbsp;&nbsp;email_body  <br>
+    from <br> 
+    &nbsp;&nbsp;emails  <br>
+    where <br>
+    &nbsp;&nbsp;date_reminder between '2022-07-01' and '2022-08-01' <br>
+    order by <br>
+    &nbsp;&nbsp;date_reminder DESC <br>
+    ;
+    </h3>
+    <h2>Results: </h2>
+    <table>
+        <tr>
+            <th>Email Id</th>
+            <th>Email Body</th>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>asdfadsf</td>
+        </tr>
+
+    </table>
+</div>
+
+<div id="flip20"><h2>Query 20: Display the names of plants that fall between 6-8 level of difficulty</h2></div>
+<div id="panel20">
+    <h2>SQL Code: </h2>
+    <h3>select <br>
+    &nbsp;&nbsp;plant_ID,  <br>
+    &nbsp;&nbsp;plant_name  <br>
+    &nbsp;&nbsp;plant_difficulty  <br>
+    from <br> 
+    &nbsp;&nbsp;plants  <br>
+    where <br>
+    &nbsp;&nbsp;plant_difficulty = '6' <br>
+    &nbsp;&nbsp;or <br>
+    &nbsp;&nbsp;plant_difficulty = '7' <br>
+    &nbsp;&nbsp;or <br>
+    &nbsp;&nbsp;plant_difficulty = '8' <br>
+    order by <br>
+    &nbsp;&nbsp;plant_difficulty ASC <br>
+    ;
+    </h3>
+    <h2>Results: </h2>
+    <table>
+        <tr>
+            <th>Plant ID</th>
+            <th>Plant Name</th>
+            <th>Plant Difficulty</th>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>African Violet</td>
+            <td>6</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Burros Tail</td>
+            <td>7</td>
+        </tr>
+        <tr>
+            <td>9</td>
+            <td>Delphinum</td>
+            <td>7</td>
+        </tr>
+        <tr>
+            <td>14</td>
+            <td>Fiddle Leaf</td>
+            <td>7</td>
+        </tr>
+        <tr>
+            <td>10</td>
+            <td>Staghorn Fern</td>
+            <td>8</td>
+        </tr>
+    </table>
+</div>
